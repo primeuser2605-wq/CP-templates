a6c8f63f4facb7d79290ea95717f113b9d576c90
primeuser2605-wq <primeuser2605@gmail.com>
2025-11-27 14:54:45 +0530

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -11,6 +11,19 @@ for (long long i = 2; i * i <= n; i++) {
     }
 }
 
+vector<int> spf(n + 1);
+for (int i = 0; i <= n; i++) spf[i] = i;
+
+for (int i = 2; i * i <= n; i++) {
+    if (spf[i] == i) { // i is prime
+        for (int j = i * i; j <= n; j += i) {
+            if (spf[j] == j)  
+                spf[j] = i;
+        }
+    }
+}
+
+
 #Advanced Version
 vector<pair<int, int>> primeFactorization(int x, vector<int>& spf){
     vector<pair<int, int>> ans;
