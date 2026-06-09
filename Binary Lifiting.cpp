029e97374d3a31671a5c3b7374f1adce05f3f3cd
primeuser2605-wq <primeuser2605@gmail.com>
2026-06-09 17:32:02 +0530

    Refactor Binary Lifting implementation

--- Binary Lifiting.cpp
+++ Binary Lifiting.cpp
@@ -1,97 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int main() {
-  int n;
-  cin >> n;
- 
-  vector<int> a(n + 1);
-  for (int i = 1; i <= n; i++) cin >> a[i];
- 
-  vector<vector<int>> adj(n + 1);
-  for (int i = 0; i < n - 1; i++) {
-    int u, v;
-    cin >> u >> v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-  }
- 
-  const int LOG = 20;  // make sure 2 ^ LOG >= n
-  vector<int> lvl(n + 1);
-  vector<vector<int>> par(n + 1, vector<int>(LOG));
-  vector<vector<int>> mx(n + 1, vector<int>(LOG));
- 
-  auto dfs = [&](auto &&dfs, int u, int p) -> void {
-    lvl[u] = lvl[p] + 1;
-    par[u][0] = p;
-    mx[u][0] = a[u];
-    for (int v : adj[u]) {
-      if (v == p) continue;
-      dfs(dfs, v, u);
+const int MAXN=200005;
+const int MAX_BITS=20;
+
+vector<int> adj[MAXN];
+int up[MAXN][MAX_BITS];
+int depth[MAXN];
+
+void dfs(int u,int p,int d){
+    depth[u]=d;
+    up[u][0]=p;
+
+    for(int i=1;i<MAX_BITS;i++){
+        up[u][i]=up[up[u][i-1]][i-1];
     }
-  };
- 
-  for (int j = 1; j < LOG; j++) {
-    for (int i = 1; i <= n; i++) {
-      int mid = par[i][j - 1];
-      par[i][j] = par[mid][j - 1];
-      mx[i][j] = max(mx[i][j - 1], mx[mid][j - 1]);
+
+    for(int v:adj[u]){
+        if(v!=p){
+            dfs(v,u,d+1);
+        }
     }
-  }
- 
-  auto lift = [&](int u, int k) {
-    for (int j = 0; j < LOG; j++) {
-      if (k >> j & 1) {
-        u = par[u][j];
-      }
+}
+
+int get_kth_ancestor(int u,int k){
+    for(int i=0;i<MAX_BITS;i++){
+        if((k>>i)&1){
+            u=up[u][i];
+        }
     }
     return u;
-  };
- 
-  auto get_max = [&](int u, int k) {
-    int res = 0;
-    for (int j = 0; j < LOG; j++) {
-      if (k >> j & 1) {
-        res = max(res, mx[u][j]);
-        u = par[u][j];
-      }
+}
+
+bool is_ancestor(int u,int v){
+    if(depth[u]>depth[v]) return false;
+
+    int diff=depth[v]-depth[u];
+    return get_kth_ancestor(v,diff)==u;
+}
+
+int get_lca(int u,int v){
+    if(depth[u]<depth[v]) swap(u,v);
+
+    u=get_kth_ancestor(u,depth[u]-depth[v]);
+
+    if(u==v) return u;
+
+    for(int i=MAX_BITS-1;i>=0;i--){
+        if(up[u][i]!=up[v][i]){
+            u=up[u][i];
+            v=up[v][i];
+        }
     }
-    return res;
-  };
- 
-  auto lca = [&](int u, int v) {
-    if (lvl[u] < lvl[v]) swap(u, v);
-    u = lift(u, lvl[u] - lvl[v]);
-    if (u == v) return u;
-    for (int j = LOG - 1; j >= 0; j--) {
-      if (par[u][j] != par[v][j]) {
-        u = par[u][j];
-        v = par[v][j];
-      }
-    }
-    return par[u][0];
-  };
- 
-  auto dist = [&](int u, int v) {
-    int l = lca(u, v);
-    return lvl[u] + lvl[v] - 2 * lvl[l];
-  };
- 
-  auto get_max_on_path = [&](int u, int v) {
-    int l = lca(u, v);
-    return max(
-        {get_max(u, lvl[u] - lvl[l]), get_max(v, lvl[v] - lvl[l]), a[l]});
-  };
- 
-  int q;
-  cin >> q;
- 
-  while (q--) {
-    int u, v;
-    cin >> u >> v;
- 
-    // Answer the queries depending on the problem.
-  }
- 
-  return 0;
+
+    return up[u][0];
 }
